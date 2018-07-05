5022626d8f951a050c7a896f61c606ed7666b029
shaileshhbv <shaileshh.bv@gmail.com>
2018-07-05 17:40:49 -0400

    adding plot and log functions

--- lp_convergence/net_evolution.py
+++ lp_convergence/net_evolution.py
@@ -18,7 +18,7 @@ class payment_network(object):
 		self.delay = delay
 		self.capacity_mat = self.credit_mat * 1.0/self.delay
 		assert np.shape(demand_mat)[0] == np.shape(demand_mat)[1]
-		assert len(graph.nodes()) = np.shape(demand_mat)[0]
+		assert len(graph.nodes()) == np.shape(demand_mat)[0]
 
 		""" initialize prices on links """
 		self.link_prices_l = {}
@@ -30,9 +30,9 @@ class payment_network(object):
 
 		for e in self.graph.edges():
 			self.link_prices_y[e[0], e[1]] = 1.
-			self.link_prices_y[e[0], e[1]] = 1.
+			self.link_prices_y[e[1], e[0]] = 1.
 			self.link_prices_z[e[0], e[1]] = 1.
-			self.link_prices_z[e[0], e[1]] = 1.			
+			self.link_prices_z[e[1], e[0]] = 1.			
 
 		""" initalize flows on paths """
 		self.total_srcdest_flow = {}
@@ -43,8 +43,8 @@ class payment_network(object):
 		for i, j in self.nonzero_demands:
 			self.total_srcdest_flow[i, j] = 0.
 			self.path_flows[i, j] = {}
-			for path in self.paths[i, j]:
-				self.path_flows[i, j][path] = 0.
+			for idx, path in enumerate(self.paths[i, j]):
+				self.path_flows[i, j][idx] = 0.
 
 		for e in self.graph.edges():
 			self.link_flows[e[0], e[1]] = 0.
@@ -72,20 +72,20 @@ class payment_network(object):
 	def update_flows(self):
 		""" update flow variables depending on link prices """
 		for i, j in self.nonzero_demands:
-			for path in self.paths[i, j]:
+			for idx, path in enumerate(self.paths[i, j]):
 				price = self.compute_path_price(path)
-				temp_flow = self.path_flows[i, j][path]
-				self.path_flows[i, j][path] += STEP_SIZE * (1. - price) # TODO: change precise update rule later
-				self.path_flows[i, j][path] = np.max([0., self.path_flows[i, j][path]])
+				temp_flow = self.path_flows[i, j][idx]
+				self.path_flows[i, j][idx] += STEP_SIZE * (1. - price) # TODO: change precise update rule later
+				self.path_flows[i, j][idx] = np.max([0., self.path_flows[i, j][idx]])
 
 				""" update total flow between i and j """
 				self.total_srcdest_flow[i, j] -= temp_flow
-				self.total_srcdest_flow[i, j] += self.path_flows[i, j][path]
+				self.total_srcdest_flow[i, j] += self.path_flows[i, j][idx]
 
 				""" update link flow states """
 				for u, v in zip(path[:-1], path[1:]):
 					self.link_flows[u, v] -= temp_flow
-					self.link_flows[u, v] += self.path_flows[i, j][path]
+					self.link_flows[u, v] += self.path_flows[i, j][idx]
 
 		# TODO: if max_num_paths is infinity, then update flow variables only on one shortest and longest path
 
@@ -109,8 +109,8 @@ class payment_network(object):
 		""" print current state of flows """
 		for i, j in self.nonzero_demands:
 			print "src: ", i, "dest: ", j
-			for path in self.paths[i, j]:
-				print "path: ", path, "flow: ", self.path_flows[i, j][path]
+			for idx, path in enumerate(self.paths[i, j]):
+				print "path: ", path, "flow: ", self.path_flows[i, j][idx]
 			print " "
 
 	def print_prices(self):
@@ -129,6 +129,27 @@ class payment_network(object):
 			print "z value for edge: ", e[1], e[0], " is ", self.link_prices_z[e[1], e[0]]
 		print " "
 
+	def print_primal_value(self):
+		""" total flow sent in current state """
+		total_flow = 0.
+		for i, j in self.nonzero_demands:
+			for idx, path in enumerate(self.paths[i, j]):
+				total_flow += self.path_flows[i, j][idx]
+
+		return total_flow
+
+	def print_dual_value(self):
+		""" total price in current state (same as dual objective) """
+		total_price = 0.
+		for i, j in self.nonzero_demands:
+			total_price += self.link_prices_l[i, j]*self.demand_mat[i, j]
+
+		for e in self.graph.edges():
+			total_price += self.link_prices_y[e[0], e[1]]*self.capacity_mat[e[0], e[1]]
+			total_price += self.link_prices_y[e[1], e[0]]*self.capacity_mat[e[1], e[0]]
+
+		return total_price
+
 def main():
 	# load ISP graph 
 	# initialize payment_network
@@ -140,18 +161,27 @@ def main():
 	demand_mat = np.ones([5, 5]) / 25.
 	np.fill_diagonal(demand_mat, 0.0)
 
-	credit_mat = np.ones([5, 5])*10
-	delay = .001
+	credit_mat = np.ones([5, 5])
+	delay = 1.
 
 	max_num_paths = 1
 
 	network = payment_network(graph, demand_mat, credit_mat, delay, max_num_paths)
 
+	primal_values = np.zeros([1, NUM_ITERATIONS])
+	dual_values = np.zeros([1, NUM_ITERATIONS])
+
 	for step in range(NUM_ITERATIONS):
 		network.update_flows()
-		network.print_flows()
+		primal_values[0, step] = network.print_primal_value()
 		network.update_prices()
-		network.print_prices()
+		dual_values[0, step] = network.print_dual_value()
+
+	print primal_values
+	print dual_values
+
+	np.save('./primal_values.npy', primal_values)	
+	np.save('./dual_values.npy', dual_values)	
 
 if __name__=='__main__':
 	main()
---
+++ lp_convergence/plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+from utils import *
+
+primal_values = np.load('./primal_values.npy')
+dual_values = np.load('./dual_values.npy')
+
+iterations = np.zeros([1, np.shape(primal_values)[1]])
+iterations[0, :] = range(np.shape(primal_values)[1])
+
+plt.plot(iterations[0, :], primal_values[0, :])
+plt.plot(iterations[0, :], dual_values[0, :])
+
+plt.show()
