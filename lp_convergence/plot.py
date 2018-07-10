658a387f1c8a2fb1af2260f492a022258484022e
shaileshhbv <shaileshh.bv@gmail.com>
2018-07-10 11:34:50 -0400

    fix typo

--- lp_convergence/plot.py
+++ lp_convergence/plot.py
@@ -14,16 +14,16 @@ cs_err_y = np.load('./cs_err_y.npy')
 cs_err_z = np.load('./cs_err_z.npy')
 cs_err_x = np.load('./cs_err_x.npy')
 
-pc_err_d = np.save('./pc_err_d.npy')
-pc_err_c = np.save('./pc_err_c.npy')
-pc_err_b = np.save('./pc_err_b.npy')
+pc_err_d = np.load('./pc_err_d.npy')
+pc_err_c = np.load('./pc_err_c.npy')
+pc_err_b = np.load('./pc_err_b.npy')
 
 iterations = np.zeros([1, np.shape(primal_values)[1]])
 iterations[0, :] = range(np.shape(primal_values)[1])
 
 """ plot """
 plt.plot(iterations[0, ::subsample], primal_values[0, ::subsample], '.', label='primal value')
-plt.plot(iterations[0, ::subsample], dual_values[0, ::subsample], '--', label='dual value')
+# plt.plot(iterations[0, ::subsample], dual_values[0, ::subsample], '--', label='dual value')
 
 # plt.plot(iterations[0, ::subsample], cs_err_l[0, ::subsample], '-s', label='cs_err_l')
 # plt.plot(iterations[0, ::subsample], cs_err_y[0, ::subsample], '-^', label='cs_err_y')
