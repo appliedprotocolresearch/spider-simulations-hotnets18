a6bcb3b437ed943e553597fdfa2ac52e01e26c89
shaileshhbv <shaileshh.bv@gmail.com>
2018-07-12 17:55:04 -0400

    standardize utils parameters for testing

--- path_based_lp_global/utils.py
+++ path_based_lp_global/utils.py
@@ -2,22 +2,22 @@
 TIME_LIMIT = 3600
 
 """ graph size """
-GRAPH_SIZE = 20
+GRAPH_SIZE = 36
 
 """ graph types: isp, scale_free """
 GRAPH_TYPE = 'isp'
 
 """ source types: uniform, skew, random_skew, random """
-SRC_TYPE = 'random'
+SRC_TYPE = 'skew'
 
 """ skew rate: real number > 0 """
-SKEW_RATE = 0.
+SKEW_RATE = 0.25
 
 """ number of paths to consider """
-MAX_NUM_PATHS = 3
+MAX_NUM_PATHS = 8
 
 """ amount of credits on edges """
-CREDIT_AMT = 6.
+CREDIT_AMT = 250.
 
 """ transaction value """
 TXN_VALUE = 1.
