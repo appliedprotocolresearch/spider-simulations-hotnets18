7952948d61fa1d7c3f8137f7fab091d4d4533774
shaileshhbv <shaileshh.bv@gmail.com>
2018-07-16 10:51:29 -0400

    adding widest paths algorithm

--- path_based_lp_global/kshortestpaths.py
+++ path_based_lp_global/kshortestpaths.py
@@ -3,6 +3,7 @@ in a graph using Yen's algorithm: https://en.wikipedia.org/wiki/Yen%27s_algorith
 
 import copy 
 import networkx as nx 
+import numpy as np
 
 def ksp_yen(graph, node_start, node_end, max_k=2):
 
@@ -96,14 +97,78 @@ def ksp_edge_disjoint(graph, node_start, node_end, max_k=2):
                 
     return A    
 
+def kwp_edge_disjoint(graph, node_start, node_end, max_k, credit_mat, delay):
+    """ compute k edge disjoint widest paths """
+    """ using http://www.cs.cmu.edu/~avrim/451f08/lectures/lect1007.pdf """
+
+    graph = copy.deepcopy(graph)
+    capacity_mat = credit_mat / delay
+    A = []
+
+    try:
+        path = nx.shortest_path(graph, source=node_start, target=node_end)
+    except:
+        print "No path found!"
+        return None 
+
+    for k in range(max_k):
+        widthto = {}
+        pathto = {}
+        tree_nodes = []
+        tree_neighbors = []
+        
+        widthto[node_end] = np.inf
+        pathto[node_end] = None
+        tree_nodes.append(node_end)
+        tree_neighbors = graph.neighbors(node_end)
+
+        while tree_neighbors and (node_start not in tree_nodes):
+            x = tree_neighbors.pop(0)
+            
+            max_width = 0.
+            max_width_neighbor = None            
+            for v in graph.neighbors(x):
+                if v in tree_nodes:
+                    if np.min([widthto[v], capacity_mat[x, v]]) > max_width:
+                        max_width = np.min([widthto[v], capacity_mat[x, v]])
+                        max_width_neighbor = v
+                else:
+                    if v not in tree_neighbors:
+                        tree_neighbors.append(v)
+
+            widthto[x] = max_width
+            pathto[x] = max_width_neighbor
+            tree_nodes.append(x)
+
+        if node_start in tree_nodes:
+            node = node_start
+            path = [node]
+            while node != node_end:
+                node = pathto[node]
+                path.append(node)
+            A.append(path)
+
+        prev_path = A[-1]
+        for i, j in zip(prev_path[:-1], prev_path[1:]):
+            if (i, j) in graph.edges() or (j, i) in graph.edges():
+                graph.remove_edge(i, j)            
+
+    return A 
+
 def main():
 
     graph = nx.Graph()
-    graph.add_nodes_from([0, 1, 2])    
-    graph.add_edges_from([(0, 1), (1, 2), (0, 2)])
-    # graph = nx.fast_gnp_random_graph(30, 0.5)
-    # print graph.edges()
-    print ksp_edge_disjoint(graph, 0, 2, 3)
+    graph.add_nodes_from([0, 1, 2, 3])    
+    graph.add_edges_from([(0, 1), (1, 2), (0, 2), (2, 3)])
+
+    credit_mat = np.zeros([4, 4])
+    credit_mat[0, 1] = 1.
+    credit_mat[1, 2] = 3.
+    credit_mat[2, 3] = 2.
+    credit_mat[0, 2] = 2.
+    delay = 1.
+
+    print kwp_edge_disjoint(graph, 0, 3, 2, credit_mat, delay)
 
 if __name__=='__main__':
     main()
