183f66e0d22c70511cafc559450f969e8d2b770b
shaileshhbv <shaileshh.bv@gmail.com>
2018-07-16 00:53:59 -0400

    edge disjoint k shortest paths

--- path_based_lp_global/kshortestpaths.py
+++ path_based_lp_global/kshortestpaths.py
@@ -66,11 +66,44 @@ def ksp_yen(graph, node_start, node_end, max_k=2):
     
     return A
 
+def ksp_edge_disjoint(graph, node_start, node_end, max_k=2):
+    """ compute k edge disjoint shortest paths """
+    graph = copy.deepcopy(graph)
+
+    A = []
+
+    try:
+        path = nx.shortest_path(graph, source=node_start, target=node_end)
+    except:
+        print "No path found!"
+        return None 
+
+    A.append(path)    
+    
+    for k in range(1, max_k):
+        prev_path = A[-1]
+        for i, j in zip(prev_path[:-1], prev_path[1:]):
+            if (i, j) in graph.edges() or (j, i) in graph.edges():
+                graph.remove_edge(i, j)
+
+        try:
+            path = nx.shortest_path(graph, source=node_start, target=node_end)
+        except:
+            path = None
+
+        if path:
+            A.append(path)
+                
+    return A    
+
 def main():
 
-    graph = nx.fast_gnp_random_graph(30, 0.5)
-    print graph.edges()
-    print ksp_yen(graph, 0, 1, 3)
+    graph = nx.Graph()
+    graph.add_nodes_from([0, 1, 2])    
+    graph.add_edges_from([(0, 1), (1, 2), (0, 2)])
+    # graph = nx.fast_gnp_random_graph(30, 0.5)
+    # print graph.edges()
+    print ksp_edge_disjoint(graph, 0, 2, 3)
 
 if __name__=='__main__':
     main()
